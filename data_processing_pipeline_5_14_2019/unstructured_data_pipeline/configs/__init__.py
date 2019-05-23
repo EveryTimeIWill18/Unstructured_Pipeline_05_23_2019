f2c23a9aaed154473e22527390845bb3539d634d
William Murphy <30298478+EveryTimeIWill18@users.noreply.github.com>
2019-05-23 13:11:12 -0400

    Create __init__.py

---
+++ data_processing_pipeline_5_14_2019/unstructured_data_pipeline/configs/__init__.py
@@ -0,0 +1,3 @@
+from unstructured_data_pipeline.configs.config_dms_claims import *
+from unstructured_data_pipeline.configs.config_sa_claims import *
+from unstructured_data_pipeline.configs.config_personal_umbrella import *
